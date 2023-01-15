958c66739a4242204c6763a30220acffb55e1dfa
Svetlana-Li <lana.lisa@mail.ru>
2023-01-15 23:39:57 +0300

    makeFriendsList

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let newUl = document.createElement('ul')
+  for (let i of friends) {
+    let newLi = document.createElement('li')
+    newLi.innerHTML = `${i.firstName} ${i.lastName}`
+    newUl.append(newLi)
+  }
+  return newUl
 }
