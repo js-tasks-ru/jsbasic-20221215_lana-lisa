4ad2eadf1d5bf41c995794fa559eb168983eaee2
Svetlana-Li <lana.lisa@mail.ru>
2023-01-19 08:24:13 +0300

    Highlight

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,31 @@
 function highlight(table) {
-  // ваш код...
+  let tbody = table.children[1]
+  for (let i = 0; i < tbody.rows.length; i++) {
+    let row = tbody.rows[i]
+    let age = row.cells[1]
+    let gender = row.cells[2]
+    let status = row.cells[3]
+
+    if (status.hasAttribute('data-available')) {
+      if (status.dataset.available ==='true') {
+        row.classList.add("available")
+      } else if (status.dataset.available ==='false') {
+        row.classList.add("unavailable")
+      }
+    }
+
+    if (!status.dataset.available) {
+      row.hidden = true
+    }
+
+    if (gender.textContent === 'm') {
+      row.classList.add('male')
+    } else if (gender.textContent === 'f') {
+      row.classList.add('female')
+    }
+
+    if (age.textContent < 18) {
+      row.style="text-decoration: line-through"
+    }
+  }
 }
