112262b6f6230dc7024d5e7afbcb092f7813b46e
Svetlana-Li <lana.lisa@mail.ru>
2023-01-26 20:26:29 +0300

    DeleteRows

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -12,7 +12,53 @@
  *      }
  *
  */
+
+
+import createElement from '../../assets/lib/create-element.js'
+
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows
+    this.elem = createElement (`
+    <table>
+        <thead>
+            <tr>
+                <th>Имя</th>
+                <th>Возраст</th>
+                <th>Зарплата</th>
+                <th>Город</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+        ${this.rows.map(i =>
+          `
+          <tr>
+            <td>${i.name}</td>
+            <td>${i.age}</td>
+            <td>${i.salary}</td>
+            <td>${i.city}</td>
+            <td><button>X</button></td>
+            </tr>
+            `
+          )}
+        </tbody>
+    </table>
+    `)
   }
+
+
+  render() {
+    this.addEventListeners()
+    return this.elem
+  }
+  
+
+  addEventListeners() {
+    for (let btn of this.elem.querySelectorAll('button')) {
+      btn.addEventListener('click',  (event)=>  {
+        event.target.closest('tr').remove()
+      })
+    }
+  } 
 }
